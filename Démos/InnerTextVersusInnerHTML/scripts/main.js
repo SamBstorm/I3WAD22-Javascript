d81e3f2da94c36b6009ee41757e1881b17c3d93a
Samuel Legrain <samuel.legrain@bstorm.be>
2022-09-07 14:13:28 +0200

    Add Démo InnerText versus InnerHTML

---
+++ Démos/InnerTextVersusInnerHTML/scripts/main.js
@@ -0,0 +1,5 @@
+const html_innerText = document.querySelector("#text");
+const html_innerHtml = document.querySelector("#html");
+
+html_innerText.innerText = "<em>Bonjour petit scarabé! Es-tu prêt pour ta <strong>lesson</strong>?</em>"
+html_innerHtml.innerHTML = "<em>Bonjour petit scarabé! Es-tu prêt pour ta <strong>lesson</strong>?</em>"
